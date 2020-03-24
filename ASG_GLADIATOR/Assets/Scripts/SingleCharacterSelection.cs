0156f29b44edcf839624eb8dedd3adb6ed97f5dd
Oğuzhan AKKAYA <oguzhanakkayaaa@gmail.com>
2020-03-24 18:21:40 +0300

    Character Selection Scene Update v2

--- ASG_GLADIATOR/Assets/Scripts/SingleCharacterSelection.cs
+++ ASG_GLADIATOR/Assets/Scripts/SingleCharacterSelection.cs
@@ -7,21 +7,27 @@ using System.IO;
 public class SingleCharacterSelection : MonoBehaviour
 {
 
-    public Button NextButton;
+    public Button NextButton, PreviousButton;
+    public Text CharacterNameText;
     public GameObject Gladiator, Cavalier, Viking;
     
 
     void Start()
     {
         NextButton = GameObject.Find("NextCharacter").GetComponent<Button>();
-       
+        PreviousButton = GameObject.Find("PreviousCharacter").GetComponent<Button>();
+        CharacterNameText= GameObject.Find("CharacterNameText").GetComponent<Text>();
+
         //Gladiator = GameObject.Find("GameObject");
         /*Cavalier = GameObject.Find("Cavalier");
         Viking = GameObject.Find("Viking");*/
 
         Gladiator.SetActive(true);
+        CharacterNameText.text = "Gladiator";
 
         NextButton.onClick.AddListener(NextCharacter);
+        PreviousButton.onClick.AddListener(PreviousCharacter);
+
     }
 
   
@@ -35,17 +41,41 @@ public class SingleCharacterSelection : MonoBehaviour
         {
             Gladiator.SetActive(false);
             Cavalier.SetActive(true);
+            CharacterNameText.text = "Cavalier";
         }
         else if(Cavalier.activeSelf== true)    
         {
             Cavalier.SetActive(false);
             Viking.SetActive(true);
+            CharacterNameText.text = "Viking";
         }
         else if (Viking.activeSelf == true)
         {
             Viking.SetActive(false);
             Gladiator.SetActive(true);
+            CharacterNameText.text = "Gladiator";
         }
 
+    }
+    public void PreviousCharacter()
+    {
+        if (Gladiator.activeSelf == true)
+        {
+            Gladiator.SetActive(false);
+            Viking.SetActive(true);
+            CharacterNameText.text = "Viking";
+        }
+        else if (Cavalier.activeSelf == true)
+        {
+            Cavalier.SetActive(false);
+            Gladiator.SetActive(true);
+            CharacterNameText.text = "Gladiator";
+        }
+        else if (Viking.activeSelf == true)
+        {
+            Viking.SetActive(false);
+            Cavalier.SetActive(true);
+            CharacterNameText.text = "Cavalier";
+        }
     }
 }
