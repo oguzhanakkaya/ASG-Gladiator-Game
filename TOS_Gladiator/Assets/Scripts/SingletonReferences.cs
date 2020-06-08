6aa049624fc895c46c1d48a1f0755eb0c2eabe86
yunusemretrkmen <yunusemretrkmen@users.noreply.github.com>
2020-06-08 20:38:43 +0300

    loby added

---
+++ TOS_Gladiator/Assets/Scripts/SingletonReferences.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonReferences : MonoBehaviour
+{
+
+    [SerializeField]
+    private MasterManager _manager;
+
+}
---
+++ TOS_Gladiator/Assets/Scripts/UI/Rooms/CurrentRoomCanvas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentRoomCanvas : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerListingsMenu _playersListingsMenu ;
+    [SerializeField]
+    private LeaveRoomMenu _leaveRoomMenu;
+    public LeaveRoomMenu LeaveRoomMenu { get { return _leaveRoomMenu; } }
+
+    private RoomsCanvases _roomsCanavases;
+
+    public void FirstInitialize(RoomsCanvases canvases)
+    {
+        _roomsCanavases = canvases;
+        _playersListingsMenu.FirstInitialize(canvases);
+        _leaveRoomMenu.FirstInitialize(canvases);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
