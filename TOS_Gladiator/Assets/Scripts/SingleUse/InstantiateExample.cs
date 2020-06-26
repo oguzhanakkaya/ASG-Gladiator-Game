98b6ccd15ff4f637c939b7bffba26970e7aed656
yunusemretrkmen <yunusemretrkmen@users.noreply.github.com>
2020-06-26 20:13:28 +0300

    Multiplayer Controller Problem Solved

--- TOS_Gladiator/Assets/Scripts/SingleUse/InstantiateExample.cs
+++ TOS_Gladiator/Assets/Scripts/SingleUse/InstantiateExample.cs
@@ -1,22 +1,80 @@
 ﻿using Photon.Pun;
 using UnityEngine;
 
-public class InstantiateExample : MonoBehaviour
+public class InstantiateExample : MonoBehaviourPun
 {
     [SerializeField]
     private GameObject _prefab;
-    
+    private GameObject _player;
+    [SerializeField]
+    private float _animationspeed = 2.0f;
+    private Animator _animator;
+
 
     private void Awake()
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            MasterManager.NetworkInstantiate(_prefab, new Vector3(-4,-2,0), Quaternion.identity);
+            _player = MasterManager.NetworkInstantiate(_prefab, new Vector3(-4,-2,0), Quaternion.identity);
             //PhotonNetwork.Instantiate("_prefab", new Vector3(-4, -2, 0), Quaternion.identity);
         }
         else
         {
-            MasterManager.NetworkInstantiate(_prefab, new Vector3(4, -2, 0), Quaternion.Euler(0,180,0));
+            _player = MasterManager.NetworkInstantiate(_prefab, new Vector3(4, -2, 0), Quaternion.Euler(0,180,0));
         }
+        _animator = _player.GetComponent<Animator>();
+    }
+    public void OnClick_WalkRight()
+    {
+
+
+        _player.transform.position += new Vector3(0.5f, 0f, 0f);
+        _animator.speed = _animationspeed;
+         _animator.Play("walkForw");
+        
+
+
+    }
+    public void OnClick_WalkLeft()
+    {
+
+
+        _player.transform.position -= new Vector3(0.5f, 0f, 0f);
+        _animator.speed = _animationspeed;
+        _animator.Play("walkBack");
+
+
+    }
+    public void OnClick_Attack1()
+    {
+
+
+        _animator.speed = _animationspeed;
+        _animator.Play("attack1");
+
+
+    }
+    public void OnClick_Attack2()
+    {
+
+        _animator.speed = _animationspeed;
+        _animator.Play("attack2");
+
+
+    }
+    public void OnClick_Attack3()
+    {
+
+
+        _animator.speed = _animationspeed;
+        _animator.Play("attack3");
+
+
+    }
+
+    private void UpdateAnimationBool(bool boolean, string animation)
+    {
+        _animator.speed = _animationspeed;
+        _animator.SetBool(animation, boolean);
     }
 }
